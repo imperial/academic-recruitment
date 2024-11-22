94fb9f56b33370f669693d36c653abb00d9cb71a
Zaki <zaki.amin20@imperial.ac.uk>
2024-11-22 16:15:06 +0000

    feat: migration to create research field and link tables

---
+++ prisma/migrations/20241122161408_add_research_field_table_and_link_table_to_application/migration.sql
@@ -0,0 +1,25 @@
+-- CreateTable
+CREATE TABLE "research_field" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "research_field_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "application_research_field" (
+    "id" SERIAL NOT NULL,
+    "application_id" INTEGER NOT NULL,
+    "research_field_id" INTEGER NOT NULL,
+
+    CONSTRAINT "application_research_field_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "research_field_name_key" ON "research_field"("name");
+
+-- AddForeignKey
+ALTER TABLE "application_research_field" ADD CONSTRAINT "application_research_field_application_id_fkey" FOREIGN KEY ("application_id") REFERENCES "application"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "application_research_field" ADD CONSTRAINT "application_research_field_research_field_id_fkey" FOREIGN KEY ("research_field_id") REFERENCES "research_field"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
