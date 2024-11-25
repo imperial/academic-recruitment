94d66c8f467e3b5b4a3a00d975b323d5af3c1274
Zaki <zaki.amin20@imperial.ac.uk>
2024-11-25 13:50:00 +0000

    fix: make homepage dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import RoundEntry from '@/components/RoundEntry'
 import { getAllApplicationRounds } from '@/lib/query'
 import { Flex, Heading } from '@radix-ui/themes'
 
+export const dynamic = 'force-dynamic'
+
 export default async function Home() {
   const allRounds = await getAllApplicationRounds()
 
