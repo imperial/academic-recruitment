a95bea9c712e77a7e4a5558874c2fe69d4d3cc4b
jsbailey <jason.bailey@imperial.ac.uk>
2024-11-26 08:06:04 +0000

    feat: add comment table migration

---
+++ prisma/migrations/20241126080233_add_comments_table/migration.sql
@@ -0,0 +1,19 @@
+-- CreateEnum
+CREATE TYPE "rating" AS ENUM ('YES', 'MAYBE', 'NO');
+
+-- CreateEnum
+CREATE TYPE "comment_type" AS ENUM ('GENERAL', 'FOLLOWING_TALK', 'INDIVIDUAL');
+
+-- CreateTable
+CREATE TABLE "comment" (
+    "id" SERIAL NOT NULL,
+    "application_id" INTEGER NOT NULL,
+    "created_date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_date" TIMESTAMP(3) NOT NULL,
+    "text" TEXT NOT NULL,
+    "author_login" TEXT NOT NULL,
+    "rating" "rating" NOT NULL,
+    "comment_type" "comment_type" NOT NULL,
+
+    CONSTRAINT "comment_pkey" PRIMARY KEY ("id")
+);
